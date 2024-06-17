5e9fedea478f3e8bacdda847f4237291dd251962
sagar parise <sagarparise01@gmail.com>
2024-06-17 16:01:24 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 3000
 //lTwebSbMUhsMGDE9
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://tech-eagle-assignment-gamma.vercel.app/"],
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   app.use(express.static(path.resolve(__dirname, "frontend", "dist" )));
